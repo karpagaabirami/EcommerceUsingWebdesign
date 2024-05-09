b584e631773f83bf27bf119a192fb5198511086f
karpagaabirami <153243782+karpagaabirami@users.noreply.github.com>
2024-05-09 12:20:25 +0530

    Add files via upload

---
+++ max/index.js
@@ -0,0 +1,16 @@
+
+var sidenav = document.querySelector(".side-navbar");
+
+function showNavbar() {
+    sidenav.style.display = "block";
+   
+}
+
+function closeNavbar() {
+    sidenav.style.display = "none";
+    
+}
+
+
+
+
